1f2aa286281132b30b56769983b146a3105a6d8b
Sophia Hon <shon@bu.edu>
2018-03-31 17:56:33 -0400

    checkbox updated

--- MusicFestivalAndroidApp/app/src/main/java/com/a5am/team/buglobalmusicfestival/MainActivity.java
+++ MusicFestivalAndroidApp/app/src/main/java/com/a5am/team/buglobalmusicfestival/MainActivity.java
@@ -13,7 +13,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -53,15 +56,18 @@ public class MainActivity extends BaseNavActivity {
 
     private ListView listView;
     private MainCalendarAdapter mcAdapter;
+    private CheckBox cb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        cb = findViewById(R.id.cb);
+
         // init database
         eventExe = new DatabaseExecute(this);
-        if (! eventExe.isDataExist()){
+        if (!eventExe.isDataExist()) {
             eventExe.initTable();
         }
 
@@ -83,7 +89,22 @@ public class MainActivity extends BaseNavActivity {
 
         mcAdapter = new MainCalendarAdapter(this, eventList);
         listView.setAdapter(mcAdapter);
+
+//        cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+//                cb.setChecked(true);
+//            }
+//
+//        });
     }
+
+//    //Checkbox
+//    public void onCheckboxClicked (View view){
+//        if (cb.isChecked()){
+//            //add to personal planner
+//        }
+//    }
 
 
     private void initComponent(){
